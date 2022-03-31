6cdef9e80005478753db284969ac7ba55afe5f7b
Nullptr <52071314+Dr-TSNG@users.noreply.github.com>
2022-03-31 12:23:37 +0800

    Only umount for denied processes

--- module/src/main/jni/zygisk.cpp
+++ module/src/main/jni/zygisk.cpp
@@ -5,10 +5,12 @@
 class Dex2oat : public zygisk::ModuleBase {
 public:
     void onLoad(zygisk::Api* api, JNIEnv* env) override {
-        umount("/apex/com.android.art/bin/dex2oat32");
-        umount("/apex/com.android.art/bin/dex2oat64");
-        umount("/system/apex/com.android.art.release/bin/dex2oat32");
-        umount("/system/apex/com.android.art.release/bin/dex2oat64");
+        if (api->getFlags() & zygisk::StateFlag::PROCESS_ON_DENYLIST) {
+            umount("/apex/com.android.art/bin/dex2oat32");
+            umount("/apex/com.android.art/bin/dex2oat64");
+            umount("/system/apex/com.android.art.release/bin/dex2oat32");
+            umount("/system/apex/com.android.art.release/bin/dex2oat64");
+        }
     }
 };
 
